34899205229e64afa07ac0ef62c1dbafd64ecb7d
Kosuke Onishi <coscosdev@gmail.com>
2023-08-27 19:20:51 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,13 @@
-<div class="h-screen w-full bg-primary-500">top contents</div>
+<div class="h-screen w-full bg-primary-500 flex items-center">
+	<div class="w-full flex items-center">
+		<div class="w-48 text-center mx-auto">
+			<p>left</p>
+		</div>
+		<div class="w-48 text-center mx-auto">
+			<p>right</p>
+		</div>
+	</div>
+</div>
 
 {#each Array(40) as _, index}
 	<p>mid contents {index}</p>
