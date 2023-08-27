0316802affc63611be6a4ba9a31c02fcea7736a4
Kosuke Onishi <coscosdev@gmail.com>
2023-08-27 19:44:31 +0900

    set up font

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,7 @@
 <div class="h-screen w-full bg-primary-500 flex items-center">
 	<div class="w-full flex items-center">
 		<div class="w-48 text-center mx-auto">
-			<p>left</p>
+			<p>left、ひだり、左</p>
 		</div>
 		<div class="w-48 text-center mx-auto">
 			<p>right</p>
@@ -10,5 +10,5 @@
 </div>
 
 {#each Array(40) as _, index}
-	<p>mid contents {index}</p>
+	<p>mid contents {index}、中央のセクション</p>
 {/each}
