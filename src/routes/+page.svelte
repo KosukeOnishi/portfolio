1f363c5df6a343608d78ee38306b997b321cd3aa
Kosuke Onishi <coscosdev@gmail.com>
2023-08-27 20:58:31 +0900

    Update +page.svelte

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -18,12 +18,23 @@
 <div class="w-fit mx-auto mt-40 px-6">
 	<div class="flex flex-col items-center">
 		<p class="w-fit text-3xl font-bold">過去のプロジェクト</p>
-		<p class="pt-10 pb-16 text-gray-500">以下のプロジェクトは、すべてFlutter製で、実際にリリースされたものです。</p>
+		<p class="pt-10 pb-16 text-gray-500 tracking-wide max-w-4xl">
+			以下のプロジェクトは、すべてFlutter製で、実際にリリースされたものです。
+		</p>
 	</div>
 </div>
 
 <div class="w-fit mx-auto mt-40 mb-32 px-6">
 	<div class="flex flex-col items-center">
 		<p class="inline-block text-3xl font-bold">About Me</p>
+		<p class="pt-10 text-gray-500 text-center leading-8 tracking-wide max-w-4xl">
+			SFC在学中に、先輩の起業したスタートアップに誘われ、Flutter開発をスタート。2つのアプリのリリース、サービスの指標設計・管理に携わりました。その会社を辞めたのち、いくつかのアプリを個人で開発しました。
+		</p>
+		<p class="pt-3 text-gray-500 text-center leading-8 tracking-wide max-w-4xl">
+			Flutter以外では、TypeScript、SQL、Pythonなどを扱えます。大学の研究として、Pythonを用いてちょっとしたシステムの実装をしました。また、Webのフロントエンドを勉強中です。（このサイトは、SvelteKitとtailwindで実装されています。）
+		</p>
+		<div class="flex pt-3 text-gray-500 text-center leading-8 tracking-wide max-w-4xl">
+			<div>Contact:</div><div class="ml-2 underline">kosuke.nsh@gmail.com</div>
+		</div>
 	</div>
 </div>
